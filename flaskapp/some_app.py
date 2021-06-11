934f5d137a2af2d559751ced88cc384ecbdcab94
A1eksey1 <85726541+A1eksey1@users.noreply.github.com>
2021-06-11 15:42:58 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -54,8 +54,8 @@ SECRET_KEY = 'secret'
 app.config['SECRET_KEY'] = SECRET_KEY
 # используем капчу и полученные секретные ключи с сайта google 
 app.config['RECAPTCHA_USE_SSL'] = False
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6LcPlh4bAAAAAPBvORhh8_dfxjVHkZnJ9fTG1GSB'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6LcPlh4bAAAAALU93D7D-WHksQ_PNompEtnbOAhL'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LcooCcbAAAAAHDqtuA6N5NXESwJp1rfqbVdTfLZ'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LcooCcbAAAAAANKJUfj5OMfWy6g2X7205rDmupY'
 app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
 # обязательно добавить для работы со стандартными шаблонами
  
