ef51152fdd6e0214ce84a7b079ce4ac266afdad5
A1eksey1 <85726541+A1eksey1@users.noreply.github.com>
2021-06-26 19:43:08 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -170,10 +170,10 @@ def krest_image(file_name, choice, choice1):
         W=str((y//3)*2)
     
     for i in range((x//2)-(int(W)//2),(x//2)+(int(W)//2)):
-        for j in range(-20,y):
+        for j in range(200,y):
             im.putpixel((i,j),(int(R),int(G),int(B)))
     
-    for i in range(20,x):
+    for i in range(100,255):
         for j in range((y//3)-(int(W)//2),(y//3)+(int(W)//2)):
             im.putpixel((i,j),(int(R),int(G),int(B)))
     im.save(file_name)
