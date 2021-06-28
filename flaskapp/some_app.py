f731f8a78a3697b6c9db810a497390e6e07fb218
A1eksey1 <85726541+A1eksey1@users.noreply.github.com>
2021-06-29 06:27:54 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -257,3 +257,21 @@ def apixml():
     # преобразуем из памяти dom в строку, возможно, понадобится указать кодировку
     strfile = ET.tostring(newhtml)
     return strfile
+   
+import tkinter as tk
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.spinbox = tk.Spinbox(self, from_=0, to=255)
+        self.scale = tk.Scale(self, from_=0, to=255,
+                              orient=tk.HORIZONTAL)
+        self.spinbox.pack()
+        self.scale.pack()
+        self.btn.pack()
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
