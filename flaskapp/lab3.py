247da21bb5d2d852b3091b0fabc626616aa653ff
A1eksey1 <85726541+A1eksey1@users.noreply.github.com>
2021-06-29 06:08:17 +0700

    Update lab3.py

--- flaskapp/lab3.py
+++ flaskapp/lab3.py
@@ -1,37 +1,42 @@
-from tkinter import Tk, BOTH, IntVar, LEFT
-from tkinter.ttk import Frame, Label, Scale, Style
- 
-class Example(Frame):
- 
+import sys
+from PyQt5 import QtWidgets, QtCore
+
+
+class Main(QtWidgets.QWidget):
+
     def __init__(self):
         super().__init__()
-        self.initUI()
- 
-    def initUI(self):
-        self.master.title("Шкала с ползунком")
-        self.style = Style()
-        self.style.theme_use("default")
- 
-        self.pack(fill=BOTH, expand=1)
- 
-        scale = Scale(self, from_=0, to=100, command=self.onScale)
-        scale.pack(side=LEFT, padx=15)
- 
-        self.var = IntVar()
-        self.label = Label(self, text=0, textvariable=self.var)
-        self.label.pack(side=LEFT)
- 
-    def onScale(self, val):
-        v = int(float(val))
-        self.var.set(v)
- 
- 
-def main():
-    root = Tk()
-    ex = Example()
-    root.geometry("250x100+300+300")
-    root.mainloop()
- 
- 
+        sld = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
+        sld.setStyleSheet("""
+            QSlider{
+                background: #E3DEE2;
+            }
+            QSlider::groove:horizontal {  
+                height: 10px;
+                margin: 0px;
+                border-radius: 5px;
+                background: #B0AEB1;
+            }
+            QSlider::handle:horizontal {
+                background: #fff;
+                border: 1px solid #E3DEE2;
+                width: 17px;
+                margin: -5px 0; 
+                border-radius: 8px;
+            }
+            QSlider::sub-page:qlineargradient {
+                background: #3B99FC;
+                border-radius: 5px;
+            }
+        """)
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(sld)
+        self.setLayout(layout)
+
+
 if __name__ == '__main__':
-    main()
+
+    app = QtWidgets.QApplication(sys.argv)
+    main = Main()
+    main.show()
+    sys.exit(app.exec_())
