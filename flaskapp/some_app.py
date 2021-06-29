2aa3b7bac75aa4319b1f7dc83bf9a163023bae98
A1eksey1 <85726541+A1eksey1@users.noreply.github.com>
2021-06-29 15:11:11 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -95,7 +95,7 @@ class IzForm(FlaskForm):
     submit = SubmitField('send')
  
  
-def krest_image(file_name, choice, choice1):
+def krest_image(file_name, file_name1, choice, choice1):
     im = Image.open(file_name)
     fig = plt.figure(figsize=(6, 4))
     ax = fig.add_subplot(1,1,1)
